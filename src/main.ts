6bb0ae7339d0c84419979879e8c1706110b93b61
ActiveDev <sarbazmahdi.2014@gmail.com>
2022-06-21 01:27:18 +0430

    implement redis io adapter as websocket adapter

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
+import { RedisIoAdapter } from './adapters/redis-io.adapter'
 import { AppModule } from './app.module'
 import { RestApiConfig } from './config/configuration'
 
@@ -12,6 +13,10 @@ async function bootstrap() {
 			logger: logger && prettyPrint ? { prettyPrint: { colorize: true } } : logger ? true : false
 		})
 	)
+
+	const redisIoAdapter = new RedisIoAdapter(app)
+	await redisIoAdapter.connectToRedis()
+	app.useWebSocketAdapter(redisIoAdapter)
 
 	const { host, port } = app.get(ConfigService).get<RestApiConfig>('server.restApi')
 

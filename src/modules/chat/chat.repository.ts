f5d9eb5c7ad0957a8cc6825c40f15bacab44f6bf
ActiveDev <sarbazmahdi.2014@gmail.com>
2022-06-26 02:41:22 +0430

    implement functions

--- src/modules/chat/chat.repository.ts
+++ src/modules/chat/chat.repository.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
 import { Model } from 'mongoose'
+import { CreateChat } from './interfaces/create-chat.interface'
 import { Chat } from './schemas/chat.schema'
 
 @Injectable()
 export class ChatRepository {
 	constructor(@InjectModel(Chat.name) private readonly model: Model<Chat>) {}
 
-	async create(users: string[]): Promise<Chat> {
-		return await new this.model({ users }).save()
+	async create(data: CreateChat): Promise<Chat> {
+		return await new this.model(data).save()
+	}
+
+	async findByPublicId(publicId: string): Promise<Chat> {
+		return await this.model.findOne({ room: { publicId } }).lean()
+	}
+
+	async findByUsersIds(usersIds: string[]): Promise<Chat[]> {
+		return await this.model.findOne({ users: { $in: usersIds } }).lean()
 	}
 }

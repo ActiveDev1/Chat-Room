db51844d0e5af97c277986596bbdc1fd479f20d1
ActiveDev <sarbazmahdi.2014@gmail.com>
2022-06-26 02:41:47 +0430

    fix create &  create createRoom func

--- src/modules/chat/chat.service.ts
+++ src/modules/chat/chat.service.ts
@@ -1,7 +1,12 @@
-import { Injectable, NotFoundException } from '@nestjs/common'
+import { Injectable, NotAcceptableException, NotFoundException } from '@nestjs/common'
+import mongoose from 'mongoose'
+import { generateRandomString } from 'src/shared/utils/functions'
 import { UserRepository } from '../user/user.repository'
 import { ChatRepository } from './chat.repository'
+import { CreateChatRoomDto } from './dtos/create-chat-room.dto'
 import { CreateChatWithUserDto } from './dtos/create-chat-with-user.dto'
+import { CreateChat } from './interfaces/create-chat.interface'
+import { Chat } from './schemas/chat.schema'
 
 @Injectable()
 export class ChatService {
@@ -10,12 +15,38 @@ export class ChatService {
 		private readonly userRepository: UserRepository
 	) {}
 
-	async create(creatorId: string, { username }: CreateChatWithUserDto) {
+	async create(creatorId: string, { username }: CreateChatWithUserDto): Promise<Chat> {
 		const user = await this.userRepository.findOneByUsername(username)
 		if (!user) {
 			throw new NotFoundException('User not found')
 		}
 
-		return await this.chatRepository.create([creatorId, user.id])
+		const usersIds = [new mongoose.Types.ObjectId(creatorId), user._id]
+		const chat = await this.chatRepository.findByUsersIds(usersIds)
+
+		if (chat) {
+			throw new NotAcceptableException('Chat with this ID is available')
+		}
+
+		return await this.chatRepository.create({ users: usersIds })
+	}
+
+	async createRoom(creatorId: string, createChatRoomDto: CreateChatRoomDto): Promise<Chat> {
+		const { name, publicId, isPrivate } = createChatRoomDto
+		const users = await this.userRepository.findByIds(createChatRoomDto.usersIds)
+		const usersIds = [...users.map((user) => user._id.toString() as string), creatorId]
+
+		if (publicId) {
+			const room = await this.chatRepository.findByPublicId(publicId)
+			if (room) {
+				throw new NotAcceptableException('Chat room with this ID is available')
+			}
+		}
+
+		const newChat: CreateChat = {
+			users: usersIds,
+			room: { name, publicId: publicId || generateRandomString(), isPrivate }
+		}
+		return await this.chatRepository.create(newChat)
 	}
 }

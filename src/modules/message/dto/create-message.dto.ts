10dc053693a5c1c30abed9c2a8d5f3e52598c6b3
ActiveDev <sarbazmahdi.2014@gmail.com>
2022-06-25 22:34:09 +0430

    add CreateMessageDto

--- src/modules/message/dto/create-message.dto.ts
+++ src/modules/message/dto/create-message.dto.ts
@@ -1,1 +1,16 @@
-export class CreateMessageDto {}
+import { IsMongoId, IsNotEmpty, IsString } from 'class-validator'
+import { Type } from 'class-transformer'
+
+class MessageContentDto {
+	@IsString()
+	@IsNotEmpty()
+	text: string
+}
+
+export class CreateMessageDto {
+	@IsMongoId()
+	chatId: string
+
+	@Type(() => MessageContentDto)
+	content: MessageContentDto
+}

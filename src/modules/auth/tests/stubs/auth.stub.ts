a34cfe95501478f9e6b6f429cb5f62eaa569473c
ActiveDev <sarbazmahdi.2014@gmail.com>
2022-07-24 21:59:16 +0430

    set refreshToken like accessToken

--- src/modules/auth/tests/stubs/auth.stub.ts
+++ src/modules/auth/tests/stubs/auth.stub.ts
@@ -3,6 +3,6 @@ import { Tokens } from '../../interfaces/token.interface'
 export const authStub = (): Tokens => {
 	return {
 		accessToken: 'as11df51wdfq',
-		refreshToken: 'asd3a6sdaqsc'
+		refreshToken: 'as11df51wdfq'
 	}
 }

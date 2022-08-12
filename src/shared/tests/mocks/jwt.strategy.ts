57ee5e6107916202cc48d1004aebd19ea614f39d
ActiveDev <sarbazmahdi.2014@gmail.com>
2022-08-12 20:42:45 +0430

    fix JwtStrategy mock

--- src/shared/tests/mocks/jwt.strategy.ts
+++ src/shared/tests/mocks/jwt.strategy.ts
@@ -1,7 +1,7 @@
 import { userStub } from '../../../modules/user/tests/stubs/user.stub'
 import { jwtPayloadStub } from '../stubs/jwt-payload.stub'
 
-export const JwtStrategy = jest.fn().mockReturnValue({
+export const JwtStrategy = {
 	verifyToken: jest.fn().mockResolvedValue(jwtPayloadStub()),
 	validate: jest.fn().mockResolvedValue(userStub())
-})
+}
